e533107e484a219f22320ef88c412237bf7d07f4
Imtiaz Uddin <59226374+Imtiaz246@users.noreply.github.com>
2025-05-28 10:43:10 +0600

    Update Validation rule in list input form (#45)
    * Add ValidationRuleObject in ListInputForm
    
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>
    
    * Add OnChange in ListInputForm
    
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>
    
    ---------
    
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>

--- types.go
+++ types.go
@@ -774,14 +774,16 @@ type SingleStepFormArrayElement struct {
 	}
 */
 type ListInputForm struct {
-	If                            string       `json:"if,omitempty"`
-	Schema                        SchemaRef    `json:"schema"`
-	Haslinelabel                  bool         `json:"hasLineLabel,omitempty"`
-	Type                          string       `json:"type"`
-	Element                       InputElement `json:"element"`
-	Label                         *Label       `json:"label"`
-	IndividualItemDisabilityCheck string       `json:"individualItemDisabilityCheck,omitempty"`
-	Required                      bool         `json:"required,omitempty"`
+	If                            string          `json:"if,omitempty"`
+	Schema                        SchemaRef       `json:"schema"`
+	Haslinelabel                  bool            `json:"hasLineLabel,omitempty"`
+	Type                          string          `json:"type"`
+	Element                       InputElement    `json:"element"`
+	Label                         *Label          `json:"label"`
+	IndividualItemDisabilityCheck string          `json:"individualItemDisabilityCheck,omitempty"`
+	Required                      bool            `json:"required,omitempty"`
+	ValidationRuleObject          *ValidationRule `json:"validationRuleObject,omitempty"`
+	OnChange                      string          `json:"onChange,omitempty"`
 }
 
 type MultiStepForm struct {
